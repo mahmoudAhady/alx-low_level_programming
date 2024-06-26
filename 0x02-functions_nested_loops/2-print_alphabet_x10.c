1a7b7b0e661c1a67f696325e2d09eed0c20f2a53
mahmoudAhady <mahmoud.a.abdulhady.ba@gmail.com>
2024-06-26 07:40:48 -0700

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,13 @@
  * Return: void
 */
 
-
 void print_alphabet_x10(void)
 {
-  int c, l;
+int c, l;
 for (l = 0; l <= 9; l++)
 
 {
-for (c = 'a'; c = 'z'; c++)
+for (c = 97; c = 122; c++)
 _putchar('c');
 _putchar('\n');
 }
