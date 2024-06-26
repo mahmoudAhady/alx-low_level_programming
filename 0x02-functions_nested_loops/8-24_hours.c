f24bf61ee360c70d67022f360e51bd0ee741a4ed
mahmoudAhady <mahmoud.a.abdulhady.ba@gmail.com>
2024-06-26 08:21:53 -0700

    c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,17 @@
 
 void jack_bauer(void)
 {
-int h, m;
+int hr, min;
 
-for (h = 0; h <= 23; h++)
+for (hr = 0; hr <= 23; h++)
 {
-for (m = 0; m <= 59; m++)
+for (min = 0; min <= 59; m++)
 
-_putchar((h / 10) + '0');
-_putchar((h % 10) + '0');
+_putchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
 _putchar(':');
-_putchar((m / 10) + '0');
-_putchar((m % 10) + '0');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
 _putchar ('\n');
 }
 }
