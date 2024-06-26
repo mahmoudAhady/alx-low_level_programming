2c109c5101e22b71bf05699ceb1f8791975dbbd2
mahmoudAhady <mahmoud.a.abdulhady.ba@gmail.com>
2024-06-26 07:38:19 -0700

    c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - print ten times
+ *
+ *c: character input
+ *
+ * Return: void
+*/
+
+
+void print_alphabet_x10(void)
+{
+  int c, l;
+for (l = 0; l <= 9; l++)
+
+{
+for (c = 'a'; c = 'z'; c++)
+_putchar('c');
+_putchar('\n');
+}
+}
