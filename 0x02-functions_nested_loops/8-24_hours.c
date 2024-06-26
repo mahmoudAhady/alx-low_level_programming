9477d68846be9d82b4b9f1420077c3ef4e05b699
mahmoudAhady <mahmoud.a.abdulhady.ba@gmail.com>
2024-06-26 08:16:24 -0700

    c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ for (h = 0; h <= 23; h++)
 {
 for (m = 0; m <= 59; m++)
 
-_putchar((h / 10) + 48);
-_putchar((h % 10) + 48);
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
 _putchar(':');
-_putchar((m / 10) + 48);
-_putchar((m % 10) + 48);
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
 _putchar ('\n');
 }
 }
