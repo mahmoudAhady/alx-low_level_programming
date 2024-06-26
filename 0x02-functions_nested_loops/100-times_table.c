4b3a9f669eb61c354f3cb6b31fd7ceaa996a968f
mahmoudAhady <mahmoud.a.abdulhady.ba@gmail.com>
2024-06-26 09:07:25 -0700

    c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 {
 int pr, num, tim;
 
-if (n <= 15 && n >=0)
+if (n <= 15 && n >= 0)
 {
 for (num = 0; num <= n; num++)
 {
@@ -24,19 +24,19 @@ pr = num * tim;
 
 if (pr <= 9)
 _putchar(' ');
-if (pr <=99)
+if (pr <= 99)
 _putchar(' ');
 
-if (pr >=100)
+if (pr >= 100)
 {
 _putchar((pr / 100) + 48);
 _putchar((pr /10) % 10 + 48);
 }
-else if (pr <=99 && pr >=10)
+else if (pr <= 99 && pr >= 10)
 _putchar((pr / 10) + 48);
 _putchar((pr % 10) + 48);
 }
- _putchar('\n');
+_putchar('\n');
 }
 }
 }
