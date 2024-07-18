373ad4ca4181c8b789399f5c3bb7add57e859208
mahmoudAhady <mahmoud.A.abdulhady.ba@gmail.com>
2024-07-17 18:18:43 -0700

    arg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- * Return: the int converted from the string
- */
+* _atoi - converts a string to an integer
+* @s: string to be converted
+*
+* Return: the int converted from the string
+*/
 int _atoi(char *s)
 {
 int i, d, n, len, f, digit;
@@ -38,11 +39,12 @@ return (0);
 return (n);
 }
 /**
-* main - multiplies two numbers		
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 (Success), 1 (Error)
- */
+* main - multiplies two numbers
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: 0 (Success), 1 (Error)
+*/
 int main(int argc, char *argv[])
 {
 int result, num1, num2;
